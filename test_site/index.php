9958ed5fb3d2ad1704ededf725b19d34cc476ae0
Don Filemon <filexpvp@gmail.com>
2020-12-16 13:24:26 +0100

    Update index.php

--- test_site/index.php
+++ test_site/index.php
@@ -66,7 +66,7 @@
                 <tbody>
                     <tr>
                         <td><a href="kwerendy.txt"></td>
-                        <td><p>Januszex Company: przebieg z oczekiwaniami klijęta </p></td>
+                        <td><p>Januszex Company: Cofanie licznika w cenie!!!</p></td>
                         <td><img src="auto.png" alt="komis samochodowy"></td>
                     </tr>
                 </tbody>
